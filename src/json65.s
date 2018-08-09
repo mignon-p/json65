e0f03f5199e7cf29e83e4b104c38d72aa79d084b
Patrick Pelletier <code@funwithsoftware.org>
2018-08-08 17:43:22 -0700

    Add enums in json65.s to match json65.h.

--- src/json65.s
+++ src/json65.s
@@ -24,6 +24,40 @@
         prop_num  = %00001000
         prop_sc   = %00000111   ; mask for structural character field
 
+;; j65_event
+.enum
+        ; events
+        J65_NULL        = 0
+        J65_FALSE       = 1
+        J65_TRUE        = 2
+        J65_INTEGER     = 3        ; integer
+        J65_NUMBER      = 4        ; string
+        J65_STRING      = 5        ; string
+        J65_KEY         = 6        ; string
+        J65_START_OBJ   = 7
+        J65_END_OBJ     = 8
+        J65_START_ARRAY = 9
+        J65_END_ARRAY   = 10
+        ; errors
+        J65_ILLEGAL_CHAR     = $80  ; integer (byte offset of error)
+        J65_ILLEGAL_ESCAPE          ; integer (byte offset of error)
+        J65_NESTING_TOO_DEEP        ; integer (byte offset of error)
+        J65_STRING_TOO_LONG         ; integer (byte offset of error)
+        J65_PARSE_ERROR             ; integer (byte offset of error)
+        J65_EXPECTED_STRING         ; integer (byte offset of error)
+        J65_EXPECTED_COLON          ; integer (byte offset of error)
+        J65_EXPECTED_COMMA          ; integer (byte offset of error)
+        J65_EXPECTED_OBJ_END        ; integer (byte offset of error)
+        J65_EXPECTED_ARRAY_END      ; integer (byte offset of error)
+.endenum
+
+;; j65_status
+.enum
+        J65_DONE      = 0
+        J65_WANT_MORE = 1
+        J65_ERROR     = 2
+.endenum
+
 ;; lexer state
 .enum
         lex_ready
